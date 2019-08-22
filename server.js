9cc29d29d667f1c6534972385cb05f7fc94b33c0
Artyom Mikhailov <happytoad42@gmail.com>
2019-08-22 22:47:23 +0700

    added User register route w/ bcrypt and JWT

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get(`/`, (req, res) =>
   res.json({ msg: 'Welcome to the Contact Keeper API' })
 );
+
+// Init middlware
+
+app.use(express.json({ extended: false }));
 
 // Define Routes
 
